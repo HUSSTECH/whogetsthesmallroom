8a93a6b3737977d3c87eb23efc66ee6f42b5d8e3
Huss <huss@husstechlabs.com>
2013-10-13 16:18:31 +0100

    edits to work with eb

--- application.py
+++ application.py
@@ -1,48 +1,48 @@
 from flask import Flask, render_template, redirect, request
 import math
 
-app = Flask(__name__)
-app.debug = True
+application = Flask(__name__)
+#application.debug = True
 
-@app.route('/', methods=['GET','POST'])
+@application.route('/', methods=['GET','POST'])
 def result():
 	if request.method == 'GET':
 		return render_template('index.html', title='WGTSR?')
 	else: #Method is Post
 
-		app.normrooms = [] #area of each room as percent
-		app.logscalerooms = [] #log of room areas
-		app.lognormrooms = [] #log adjusted area of each room as percent
-		app.price = [] #rent for each room based on all of this stuff
-		app.rent = request.form["rent"] #total rent
-		app.rooms = request.form.values() #post request from the form
-		app.rooms.remove(app.rent) #remove the rent from list
-		app.rooms.sort() 
-		app.rooms.reverse()
+		application.normrooms = [] #area of each room as percent
+		application.logscalerooms = [] #log of room areas
+		application.lognormrooms = [] #log adjusted area of each room as percent
+		application.price = [] #rent for each room based on all of this stuff
+		application.rent = request.form["rent"] #total rent
+		application.rooms = request.form.values() #post request from the form
+		application.rooms.remove(application.rent) #remove the rent from list
+		application.rooms.sort() 
+		application.rooms.reverse()
 
-		app.rent = float(app.rent)
-		app.rooms = [float(i) for i in app.rooms]
+		application.rent = float(application.rent)
+		application.rooms = [float(i) for i in application.rooms]
 
-		app.nrooms = len(app.rooms) #number of rooms
-		app.sumrooms = sum(app.rooms) #total area of rooms
+		application.nrooms = len(application.rooms) #number of rooms
+		application.sumrooms = sum(application.rooms) #total area of rooms
 
-		for x in app.rooms: #normalise room area
-			app.normrooms.append(x/app.sumrooms)
-		for x in app.normrooms: #log of the normalised room areas
-			app.logscalerooms.append(math.log10(x*app.rent))
-		for x in app.logscalerooms: #calc log scale factor for rooms
-			app.lognormrooms.append(x/sum(app.logscalerooms))
-		for x in app.lognormrooms: #do the scaling
-			app.price.append(round(x*app.rent,2))
+		for x in application.rooms: #normalise room area
+			application.normrooms.append(x/application.sumrooms)
+		for x in application.normrooms: #log of the normalised room areas
+			application.logscalerooms.append(math.log10(x*application.rent))
+		for x in application.logscalerooms: #calc log scale factor for rooms
+			application.lognormrooms.append(x/sum(application.logscalerooms))
+		for x in application.lognormrooms: #do the scaling
+			application.price.append(round(x*application.rent,2))
 
-		app.price.sort()
-		app.price.reverse()
+		application.price.sort()
+		application.price.reverse()
 
-		return render_template('index2.html', title='WGTSR?',rooms=app.rooms,rent=app.rent,price=app.price)
+		return render_template('index2.html', title='WGTSR?',rooms=application.rooms,rent=application.rent,price=application.price)
 
-@app.route('/about')
+@application.route('/about')
 def about():
 	return render_template('about.html', title='About')
 
 if __name__ == '__main__':
-    app.run()
+    application.run(host='0.0.0.0')
